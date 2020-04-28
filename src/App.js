b418a5ea7d4741a405be85e1537270f925ae3627
Patrick <patrickmarin@gmail.com>
2020-04-28 06:55:16 -0500

    picSome

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+import {Switch, Link,Route} from "react-router-dom"
+
+function App() {    
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path= "/"> <Photos/></Route>
+                <Route path= "/cart"> <Cart/></Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default App
---
+++ src/pages/Photos.js
@@ -0,0 +1,19 @@
+import React, {useContext} from "react"
+import Image from "../components/Image"
+import {getClass} from "../utils"
+import {Context} from "../Context"
+
+function Photos() {
+
+ const{allPhotos} = useContext(Context)
+ const imageElements = allPhotos.map((img,i) => (
+     <Image key = {img.id} img={img} className={getClass(i)}/>
+ ))
+    return (
+        <main className="photos">
+            {imageElements}
+        </main>
+    )
+}
+
+export default Photos
